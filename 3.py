c7b203a58d1be919e65a92c96ed43193089afd7d
KosenokIvan <kosenoki213@gmail.com>
2024-03-15 23:06:06 +0300

    Changed program input in 3 task

--- 3.py
+++ 3.py
@@ -1,28 +1,26 @@
 from stack import Stack
 
 
-def hanoi(stacks_, from_index, to_index, n):
+def hanoi(stacks_, from_index, to_index, n, result_str):
     if n <= 1:
         stacks_[to_index].append(stacks_[from_index].pop())
+        result_str += f"Move disk from {from_index + 1} to {to_index + 1}\n"
+        return result_str
     else:
         indexes = list(range(3))
         indexes.remove(from_index)
         indexes.remove(to_index)
         temp_index = indexes[0]
-        hanoi(stacks_, from_index, temp_index, n - 1)
-        hanoi(stacks_, from_index, to_index, 1)
-        hanoi(stacks_, temp_index, to_index, n - 1)
+        result_str = hanoi(stacks_, from_index, temp_index, n - 1, result_str)
+        result_str = hanoi(stacks_, from_index, to_index, 1, result_str)
+        result_str = hanoi(stacks_, temp_index, to_index, n - 1, result_str)
+        return result_str
 
 
 with open("hanoi3.txt") as in_file:
     disks = sorted([int(line) for line in in_file.readlines() if line])
 stacks = Stack(disks), Stack(), Stack()
-hanoi(stacks, 0, 2, len(disks))
-result = ""
-for stack in stacks:
-    while not stack.is_empty():
-        result += str(stack.pop()) + " "
-    result += "\n"
+result = hanoi(stacks, 0, 2, len(disks), "")
 print(result)
 with open("result3.txt", "w") as out_file:
     out_file.write(result)
